b4ef436029c2ae4488244bf3bccfdfa0b6ae7b38
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-26 23:22:09 +0200

    Added method `timeout`

--- utils/api.ts
+++ utils/api.ts
@@ -1,2 +1,3 @@
 export const fetcher = (input: RequestInfo | URL, init: RequestInit) => fetch(input, init).then(res => res.json());
 export const buildImageUrl = (path: string, size: string = "original") => `https://image.tmdb.org/t/p/${size}${path}`;
+export const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
