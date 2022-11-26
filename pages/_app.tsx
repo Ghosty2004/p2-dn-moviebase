f4817c9e7e5bc4194afc93547058a601b4ce0edf
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-26 23:22:50 +0200

    Now the user will be fetched from here

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,29 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../utils/theme';
 
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import { user } from '../utils/interfaces';
+import { validateUser } from '../utils/methods/user';
 
 export default function App({ Component, pageProps }: AppProps) {
+    const [user, setUser] = useState<user | null>();
+    
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const result = await validateUser(window.localStorage.getItem("token") as string);
+                if(typeof(result.error) === "undefined") setUser(result);
+                else setUser(null);
+            } catch {}
+        };
+        init();
+    }, []);
+
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            {typeof(user) !== "undefined" ? (
+                <Component {...pageProps} user={user} />
+            ): null}
         </ChakraProvider>
     );
 }
