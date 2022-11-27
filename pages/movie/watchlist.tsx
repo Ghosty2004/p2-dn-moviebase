a3e46aa36ee9ed27f13609571d8a6d672e3084be
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-27 21:33:35 +0200

    Redirect to `/user/login` if it's not logged

--- pages/movie/watchlist.tsx
+++ pages/movie/watchlist.tsx
@@ -1,7 +1,10 @@
+import Router from "next/router";
 import Layout from "../../components/Layout";
 import { DefaultProps } from "../../utils/types";
 
 export default function WatchList({ user }: DefaultProps): JSX.Element {
+    if(!user) Router.push("/user/login");
+
     return (
         <Layout title="Your watchlist" user={user}>
             
