40ced02c799e8962936e03c576baf45bbeb0c93c
Ghosty2004 <fejerszilveszter@gmail.com>
2022-12-01 19:09:23 +0200

    Added method `RenderActions` where you will be able to add to watch list

--- pages/movie/details/[id].tsx
+++ pages/movie/details/[id].tsx
@@ -1,15 +1,47 @@
 import Image from "next/image";
-import { Badge, Box, Center, CircularProgress, Container, Heading, HStack, Spacer, Stack, Tag, Text, Tooltip } from "@chakra-ui/react";
+import { Badge, Box, Button, Center, CircularProgress, Container, Heading, HStack, Icon, Stack, Tag, Text, Tooltip, useToast } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 import Layout from "../../../components/Layout";
 import { buildFlagImageUrl, buildImageUrl, fetcher } from "../../../utils/api";
 import { ERROR_UNEXPECTED } from "../../../utils/errors";
 import { DefaultProps, MovieDetailsData } from "../../../utils/types";
+import { FaExternalLinkAlt, FaFlag, FaMinus, FaPlus, FaSpeakap, FaStar } from "react-icons/fa";
+import Link from "next/link";
+import { addUserHistoryList, toggleUserWatchList } from "../../../utils/methods/user";
+
+function RenderActions({ user, movieId }: DefaultProps & { movieId: number }): JSX.Element {
+    const [isInWatchList, setIsInWatchList] = useState<boolean>(user?.watchList?.includes(movieId) || false);
+    const toast = useToast({ position: "bottom-right", isClosable: true });
+
+    return (
+        <>
+            {user ? (
+                <Box>
+                    <Button size="sm" colorScheme={isInWatchList ? "red" : "green"} leftIcon={isInWatchList ? <FaMinus /> : <FaPlus />} onClick={() => {
+                        const toastId = toast({ title: `${isInWatchList ? "Removing from watch list" : "Adding to watch list"}...`, status: "loading" });
+                        toggleUserWatchList((window.localStorage.getItem("token") as string), movieId).then((response) => {
+                            if(response.error) return;
+                            setIsInWatchList(response.status === "added");
+                            toast.update(toastId, { title: `${response.status === "added" ? "Added to watch list" : "Removed from watch list"}`, status: "success" });
+                        });
+                    }}>{isInWatchList ? "Remove from watchlist" : "Add to watchlist"}</Button>
+                </Box>
+            ): null}
+        </>
+    );
+};
 
 export default function Details({ user }: DefaultProps): JSX.Element {
     const { id } = useRouter().query;
     const { data, error } = useSWR<MovieDetailsData>(`/api/movie/details/?id=${id}`, fetcher);
+
+    useEffect(() => {
+        if(!error && data?.id && user) {
+            addUserHistoryList((localStorage.getItem("token") as string), data.id);
+        }
+    });
 
     return (
         <Layout title={data?.title || "Not found"} user={user}>
@@ -54,15 +86,41 @@ export default function Details({ user }: DefaultProps): JSX.Element {
                                             {genre.name}
                                         </Badge>
                                     ))}
-                                    <Spacer />
+                                </>
+                            </Stack>
+                            <Box>{data.overview}</Box>
+
+                            <Box cursor="default" border="2px"borderColor="blue.300"  p="10px">
+                                <HStack>
+                                    <Tooltip label={`${data.spoken_languages.length} language(s)`}><Badge><Icon as={FaSpeakap} /> Available in:</Badge></Tooltip>
                                     {data.spoken_languages.map((language, index) => (
                                         <Tooltip label={language.english_name} key={index}>
                                             <img src={buildFlagImageUrl(language.iso_639_1)} alt={language.english_name} width="24" height="24" key={index} />
                                         </Tooltip>
                                     ))}
-                                </>
-                            </Stack>
-                            <Box>{data.overview}</Box>
+                                </HStack>
+
+                                <HStack pt="3px">
+                                    <Badge><Icon as={FaFlag} /> Original language:</Badge>
+                                    <Tooltip label={data.original_language}>
+                                        <img src={buildFlagImageUrl(data.original_language)} alt={data.original_language} width="24" height="24" />
+                                    </Tooltip>
+                                </HStack>
+
+                                <HStack>
+                                    <Badge><Icon as={FaStar} /> Rating:</Badge>
+                                    <Text>{data.vote_count}</Text>
+                                </HStack>
+
+                                {data.homepage.length ? (
+                                    <HStack>
+                                        <Badge><Icon as={FaExternalLinkAlt} /> Home page:</Badge>
+                                        <Text><Link href={data.homepage}>{data.homepage}</Link></Text>
+                                    </HStack>
+                                ): null}
+                            </Box>
+
+                            <RenderActions user={user} movieId={data.id} />
                         </Stack>
                     </Stack>
                 )}
