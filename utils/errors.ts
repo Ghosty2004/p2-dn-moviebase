8f756255a035a8e3229020eda88cf941fb24e7c2
Ghosty2004 <fejerszilveszter@gmail.com>
2022-12-01 19:08:56 +0200

    Added `ERROR_INVALID_AUTHORIZATION`

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,2 +1,3 @@
 export const ERROR_UNEXPECTED = "An unexpected error occurred. Please try again later.";
 export const ERROR_METHOD_NOT_ALLOWED = "Sorry, but this method is not allowed.";
+export const ERROR_INVALID_AUTHORIZATION = "Invalid authorization.";
