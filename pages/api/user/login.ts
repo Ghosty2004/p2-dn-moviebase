9d0a3bb61a9553ad9a54af796dc3da622aa4e756
Ghosty2004 <fejerszilveszter@gmail.com>
2022-12-15 18:43:38 +0200

    Now the password will be crypted after register

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import md5 from "md5";
 import { users } from "../../../utils/db";
 import { ERROR_METHOD_NOT_ALLOWED, ERROR_UNEXPECTED } from "../../../utils/errors";
 import { userLoginResonse } from "../../../utils/types";
@@ -6,6 +7,7 @@ import { userLoginResonse } from "../../../utils/types";
 export default async function handler(request: NextApiRequest, response: NextApiResponse<userLoginResonse>) {
     if(request.method !== "POST") return response.json({ error: true, message: ERROR_METHOD_NOT_ALLOWED });
     try {
+        if(typeof(request.body.password) !== "undefined") request.body.password = md5(request.body.password);
         const { name, password } = request.body;
         const user = await users.findOne({ name, password });
         if(!user) return response.json({ error: true, message: "Invalid credentials!" });
--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import md5 from "md5";
 import { users } from "../../../utils/db";
 import { ERROR_METHOD_NOT_ALLOWED, ERROR_UNEXPECTED } from "../../../utils/errors";
 import { createRandomToken } from "../../../utils/api";
@@ -7,6 +8,7 @@ import { userRegisterResponse } from "../../../utils/types";
 export default async function handler(request: NextApiRequest, response: NextApiResponse<userRegisterResponse>) {
     if(request.method !== "POST") return response.json({ error: true, message: ERROR_METHOD_NOT_ALLOWED });
     try {
+        if(typeof(request.body.password) !== "undefined") request.body.password = md5(request.body.password);
         const { name, eMail, password } = request.body;
         if(await(users.exists({ $or: [{ name }, { eMail }] }))) return response.json({ error: true, message: "This user is already registered." });
         const token = createRandomToken(20);
